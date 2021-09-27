01ff004b95014f0a3b35cfbfc55fb61c67a81354
humoyun.norboboev <humoyun.norboboev@gmail.com>
2021-09-27 20:45:49 +0500

    Auditable

--- src/main/java/uz/personal/config/HibernateConfig.java
+++ src/main/java/uz/personal/config/HibernateConfig.java
@@ -6,33 +6,50 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import uz.personal.utils.UserSession;
 
 import javax.persistence.EntityManagerFactory;
+import java.util.Optional;
 
 @Configuration
 @PropertySource(value = {"classpath:application.properties", "classpath:hibernate.properties"})
 @PropertySource(name = "hibernate", value = "classpath:hibernate.properties")
 @EnableTransactionManagement
-public class HibernateConfig {
+@EnableJpaAuditing
+public class HibernateConfig implements AuditorAware<Long> {
 
-    @Bean
-    public Module hibernate5Module() {
-        return new Hibernate5Module();
-    }
+  private final UserSession userSession;
 
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(emf);
+  public HibernateConfig(UserSession userSession) {
+    this.userSession = userSession;
+  }
 
-        return transactionManager;
-    }
+  @Bean
+  public Module hibernate5Module() {
+    return new Hibernate5Module();
+  }
 
-    @Bean
-    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-        return new PersistenceExceptionTranslationPostProcessor();
-    }
+  @Bean
+  public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+    JpaTransactionManager transactionManager = new JpaTransactionManager();
+    transactionManager.setEntityManagerFactory(emf);
+
+    return transactionManager;
+  }
+
+  @Bean
+  public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+    return new PersistenceExceptionTranslationPostProcessor();
+  }
+
+  @Override
+  public Optional<Long> getCurrentAuditor() {
+    return Optional.ofNullable(
+        userSession.getUser() == null ? null : userSession.getUser().getId());
+  }
 }
